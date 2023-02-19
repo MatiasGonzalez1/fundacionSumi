477c9a3989c48172e5e827fcc63c2a21a1b778c8
Matias Gonzalez <mng_epson@hotmail.com>
2023-02-19 13:10:12 -0300

    feat: add component folder and utils folder

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from '../../utils/Link'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='logo'>Logo</div>
+      <ul className='nav'>
+     <Link 
+     name='Nosotros'/>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
