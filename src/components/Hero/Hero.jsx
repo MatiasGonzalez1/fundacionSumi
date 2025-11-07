57e9b24d285d27c781b23d8b8584f310d711b3a3
Matias Gonzalez <mng_epson@hotmail.com>
2025-11-07 18:32:40 -0300

    add: se agrega botón de contacto al hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
-import HeroImage from "../../assets/Cataratas-del-lado-argentino.jpg"; // Asegúrate de tener esta imagen en tu proyecto
+import HeroImage from "../../assets/Cataratas-del-lado-argentino.jpg";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <section className="relative py-20 sm:py-16 md:py-20 bg-gradient-to-r from-blue-50/20 to-purple-50/20 bg-slate-100 min-h-screen flex items-center">
       <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-12 flex flex-col md:flex-row items-center">
-        {/* Texto */}
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
@@ -18,8 +18,6 @@ const Hero = () => {
           <h2 className="mt-3 text-sm sm:text-base md:text-lg lg:text-xl text-gray-700 font-semibold text-justify">
          Nos dedicamos a promover la cultura, acompañar a las comunidades y generar alianzas con proyectos estratégicos</h2>
           <p className="mt-4 text-xs sm:text-sm md:text-base text-gray-600">
-            {/* Únete a nuestra misión: fomentar la cultura, apoyar a quienes más lo necesitan */}
-            {/* y construir un futuro basado en valores y colaboración. */}
             Misión: articular recursos, conocimientos y capacidades para el desarrollo de programas sociales que respondan a las necesidades reales de las comunidades y su entorno.
           </p>
 
@@ -40,10 +38,25 @@ const Hero = () => {
             <li className="flex items-center">
               ✅ <span className="ml-2 text-xs sm:text-sm">Facilitamos procesos: Vinculos estratégicos para fortalecer el desarrollo individual y colectivo.</span>
             </li>           
-
-
           </ul>
+
+          <motion.div
+  initial={{ opacity: 0, y: 20 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.8, delay: 0.4 }} // Un pequeño delay para que aparezca después del texto
+  className="mt-8 text-center md:text-left" // 'mt-8' para darle espacio
+>
+  <Link
+    to="/contact"
+    className="inline-block px-6 py-3 text-base font-semibold text-white bg-blue-700 rounded-lg shadow-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300"
+  >
+    ¡Quiero ser voluntario!
+  </Link>
+</motion.div>
         </motion.div>
+
+        
+        
 
         {/* Imagen */}
         <motion.div
@@ -54,7 +67,7 @@ const Hero = () => {
         >
           <img
             src={HeroImage}
-            alt="Héroes comunitarios"
+            alt="Cataratas del Iguazú, Misiones"
             className="w-[40vw] sm:w-[35vw] md:w-[30vw] lg:w-[25vw] drop-shadow-lg rounded-2xl max-w-[600px]"
           />
         </motion.div>
